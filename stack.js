09e6fa8ffe335ca2b1393c9dfa2dcbd033821328
nadia-elm <elmefeddelnadia@gmail.com>
2023-11-02 10:15:06 +0400

    commit

---
+++ stack.js
@@ -0,0 +1,67 @@
+/** Node: node for a stack. */
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+/** Stack: chained-together nodes where you can
+ *  remove from the top or add to the top. */
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  /** push(val): add new value to end of the stack. Returns undefined. */
+
+  push(val) {
+    const node = new Node(val);
+    if (!this.first && !this.last) {
+      this.first = node;
+      this.last = node;
+    } else {
+      node.next = this.first;
+      this.first = node;
+    }
+    this.size += 1;
+  }
+
+  /** pop(): remove the node from the top of the stack
+   * and return its value. Should throw an error if the stack is empty. */
+
+  pop() {
+    if (this.size === 0) {
+      throw new Error('List cannot be empty');
+    } else if (this.size === 1) {
+      let returnVal = this.first.val;
+      this.last = null;
+      this.first = null;
+      this.size -= 1;
+      return returnVal;
+    } else {
+      let returnVal = this.first.val;
+      this.first = this.first.next;
+      this.size -= 1;
+      return returnVal;
+    }
+  }
+
+  /** peek(): return the value of the first node in the stack. */
+
+  peek() {
+    return this.first.val;
+  }
+
+  /** isEmpty(): return true if the stack is empty, otherwise false */
+
+  isEmpty() {
+    return this.size === 0;
+  }
+}
+
+module.exports = Stack;
